ab5b9c9bb90ddeba14b89425e1fe37d114765e44
EmmyRocky <emmyrockyy@gmail.com>
2022-04-10 07:26:49 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * see lets make this task 5 work
+ *
+ * we will be successful
+ */
+int main(void)
+
+{
+int i;
+
+for (i = 0 ; i < 10; i++)
+{
+printf("%i", i);
+}
+
+printf("\n");
+
+return (0);
+
+}
+
