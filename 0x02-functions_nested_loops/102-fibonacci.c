13cd29d64e48af0bc4cf7c1a1df9d94a0811ded7
EmmyRocky <emmyrockyy@gmail.com>
2022-04-16 11:22:00 -0700

    task 14 adv

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
-
 /**
  * main - main function
  *
  * Return: end program
  */
-
 int main(void)
-
 {
-long int n, a =1, b = 1, s;
-int c = 1;
+	long int n, a = 1, b =1, s;
 
-printf("%ld", a);
-for (n = 1; n < 50; ++n)
+	printf("%ld", a);
+	for (n = 1; n < 50; ++n)
+	{
+		s = a + b;
+		printf(", %ld", s);
+		a = b;
+		b = s;
+	}
+	putchar(10);
 
-{
-s = a + b;
-printf(", %ld", s);
-a = b;
-b = s;
-
-}
-putchar(10);
-
-return (0);
+	return (0);
 }
