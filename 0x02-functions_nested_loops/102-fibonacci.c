68225ca821a7f37ca74105425cc32ab358b50bf8
EmmyRocky <emmyrockyy@gmail.com>
2022-04-16 11:24:18 -0700

    task 14 adv

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-	long int n, a = 1, b =1, s;
+long int n, a = 1, b =1, s;
 
-	printf("%ld", a);
-	for (n = 1; n < 50; ++n)
-	{
-		s = a + b;
-		printf(", %ld", s);
-		a = b;
-		b = s;
-	}
-	putchar(10);
+printf("%ld", a);
+for (n = 1; n < 50; ++n)
+{
+s = a + b;
+printf(", %ld", s);
+a = b;
+b = s;
+}
+putchar(10);
 
-	return (0);
+return (0);
 }
