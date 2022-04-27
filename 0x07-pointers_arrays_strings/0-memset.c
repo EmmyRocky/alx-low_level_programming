ea88dcca5486a66594bd10c9dd82fe0dbe41c4e2
EmmyRocky <emmyrockyy@gmail.com>
2022-04-27 12:58:12 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - should fill memory with a constant byte
+ * @s: value to declare
+ * @b: size of the memory
+ * @n: bytes Constant
+ *
+ * Return: to the Destination
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int j;
+
+	for (j=0; j < n; j++)
+	{
+		*(s+j) = b; /*insertion of 1 position s*/
+	} /*Ending keyword for*/
+	return (s);
+}
