8aedb0fba49ea14365c445b804d300862e0f0c4c
EmmyRocky <emmyrockyy@gmail.com>
2022-05-11 13:21:51 -0700

    task 0

--- dog.h
+++ dog.h
@@ -15,6 +15,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
@@ -22,4 +23,5 @@ typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
