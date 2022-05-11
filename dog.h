cd6f09895e8928c704723dbf64a3249268944c1e
EmmyRocky <emmyrockyy@gmail.com>
2022-05-11 13:19:51 -0700

    task 0

---
+++ dog.h
@@ -0,0 +1,25 @@
+#ifndef  HEADER_FILE
+#define HEADER_FILE
+/**
+ * struct dog - This generates the structure dog
+ *
+ * @name: member number one
+ * @age: member number two
+ * @owner: member number three
+ *
+ * Description: Generates the structure dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
