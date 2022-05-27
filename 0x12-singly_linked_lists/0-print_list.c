71dc1fe1d71e06cc6d235628d4d20c00bb1f45e9
EmmyRocky <emmyrockyy@gmail.com>
2022-05-27 08:16:01 -0700

    0-*print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,11 @@
 #include "lists.h"
+
 /**
  * print_list - function that prints all the elements of a list_t list.
- *
- * @h: Parameter with the list
- *
+ * @h: header pointer
  * Return: the number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t i;
