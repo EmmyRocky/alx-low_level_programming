5a3ee90b66f23c1cc8e99e7f98bc47a2b485f2cb
EmmyRocky <emmyrockyy@gmail.com>
2022-06-17 03:45:49 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,18 +1,54 @@
 #include "main.h"
-#include <stdio.h>
 
+/**
+ * long_1 - main - check the code
+ * @s: number
+ *
+ * Return: Always 0.
+ */
 
-int reverser(char *s, char *a, int l)
+int long_1(char *s)
 {
 	if (*s != '\0')
 	{
-		reverser(s + 1, a, l + 1);
-		if (l >= 0);
-		printf("%c", a[l]);
+		return (1 + long_1(s + 1));
+	}
+	return (0);
+
+}
+/**
+ * compare - main - check the code
+ * @s: number
+ * @l: lenght
+ *
+ * Return: Always 0.
+ */
+int compare(char *s, int l)
+{
+	if (l <= 0)
+	{
+		return (1);
+	}
+	if (*s == *(s + (l - 1)))
+	{
+		return (compare(s + 1, l - 2));
+	}
+	else
+	{
+		return (0);
 	}
 }
 
-int main()
+/**
+ * is_palindrome - main - check the code
+ * @s: character
+ *
+ * Return: Always 0.
+ */
+int is_palindrome(char *s)
 {
-	reverser("level", "", 0);
+int l;
+
+	l = long_1(s);
+	return (compare(s, l));
 }
